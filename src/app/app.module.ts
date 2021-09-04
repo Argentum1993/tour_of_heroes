9c86ce044a0f00f338c1d56c343f2eb09aa69806
Argentum1993 <hlopec1993@gmail.com>
2021-09-04 22:48:31 +0300

    Implement search component
    * Create template
    
    * Implement methods on component for fetch heroes when user input term
    
    * Put hero-search.component to dashboard.component
    
    * Create CSS styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
 import { InMemHeroService } from './in-mem-hero.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { InMemHeroService } from './in-mem-hero.service';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
