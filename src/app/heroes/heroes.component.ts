66a1fa0ade7123ecd12074f241ca2f1f929ba741
Argentum1993 <hlopec1993@gmail.com>
2021-09-04 20:00:41 +0300

    Implement add hero feature

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -15,5 +15,18 @@ export class HeroesComponent implements OnInit {
   constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit() {
+    this.heroService.getHeroes()
+      .subscribe( heroes => {
+        this.heroes = heroes
+      });
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
   }
 }
